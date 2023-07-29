2173f77fda532b95f20826671eda56baeefc7072
Eduardo Gallego <eduardo.gallego@bluu.es>
2023-07-29 03:25:49 +0200

    Startup

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from utils import Config
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+config = Config()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    print('Hello')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 import json
-import logging
-import logging.handlers
 
 
 class Config:
@@ -11,44 +9,3 @@ class Config:
 
     def get(self, parameter):
         return self._config.get(parameter)
-
-
-class Logger:
-    def __init__(self):
-        handler = logging.handlers.WatchedFileHandler('fantasy.log')
-        handler.setFormatter(logging.Formatter(
-            fmt="%(asctime)s.%(msecs)03d - %(levelname)s [%(threadName)s]- %(name)s: %(message)s",
-            datefmt="%d/%b/%Y %H:%M:%S"))
-        root = logging.getLogger()
-        root.setLevel(logging.INFO)
-        root.addHandler(handler)
-
-
-class User:
-    def __init__(self, user_id, user_name, user_password):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.user_password = user_password
-        self.authenticated = False
-
-    def is_authenticated(self):
-        return self.authenticated
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.user_id
-
-    def get_user_name(self):
-        return self.user_name
-
-    def login(self, password):
-        self.authenticated = (password == self.user_password)
-        return self.authenticated
-
-    def logout(self):
-        self.authenticated = False
